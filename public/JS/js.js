3dad8579c0176fe937c4e33845b579e775e7281c
marcigarza <marc.igarza@est.fib.upc.edu>
2017-12-10 22:26:51 +0100

    big updates

--- public/JS/js.js
+++ public/JS/js.js
@@ -15,28 +15,22 @@ socket.on('messages', function(data) {
 })
 
 function render (data) {
-  //var post = querystring.parse(data);
-  var parseString = require('xml2js').parseString;
-  var xml = data;
   console.log("PRINT de XML despues del QueryString");
   console.log(xml);
-  parseString(xml, function (err, result) {
-    //result is a JSON with all the data. {module_x {radon : [n],}}
-    console.log("PRINT de JSON despues del ParseString");
-    console.log(result);
-    if (JSON.stringify(result.module_1) != undefined) {
-      radon_value_module_1 = JSON.stringify(result.module_1.radon);
-      co2_value_module_1 = JSON.stringify(result.module_1.co2);
-      methane_value_module_1 = JSON.stringify(result.module_1.methane);
-    }
-
-    if (JSON.stringify(result.module_2) != undefined) {
-      radon_value_module_2 = JSON.stringify(result.module_2.radon);
-      co2_value_module_2 = JSON.stringify(result.module_2.co2);
-      methane_value_module_2 = JSON.stringify(result.module_2.methane);
-
-    }
-  });
+  var result = xmlToJson(data);
+  //result is a JSON with all the data. {module_x {radon : [n],}}
+  console.log("PRINT de JSON despues del ParseString");
+  console.log(result);
+  if (JSON.stringify(result.module_1) != undefined) {
+    radon_value_module_1 = JSON.stringify(result.module_1.radon);
+    co2_value_module_1 = JSON.stringify(result.module_1.co2);
+    methane_value_module_1 = JSON.stringify(result.module_1.methane);
+  }
+  if (JSON.stringify(result.module_2) != undefined) {
+    radon_value_module_2 = JSON.stringify(result.module_2.radon);
+    co2_value_module_2 = JSON.stringify(result.module_2.co2);
+    methane_value_module_2 = JSON.stringify(result.module_2.methane);
+  }
 
   document.getElementById("radon_value_module_1").innerHTML = radon_value_module_1;
   document.getElementById("co2_value_module_1").innerHTML = co2_value_module_1;
@@ -45,3 +39,42 @@ function render (data) {
   document.getElementById("co2_value_module_2").innerHTML = co2_value_module_2;
   document.getElementById("methane_value_module_2").innerHTML = methane_value_module_2;
 }
+
+
+function xmlToJson( xml ) {
+
+  // Create the return object
+  var obj = {};
+
+  if ( xml.nodeType == 1 ) { // element
+    // do attributes
+    if ( xml.attributes.length > 0 ) {
+    obj["@attributes"] = {};
+      for ( var j = 0; j < xml.attributes.length; j++ ) {
+        var attribute = xml.attributes.item( j );
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if ( xml.nodeType == 3 ) { // text
+    obj = xml.nodeValue;
+  }
+
+  // do children
+  if ( xml.hasChildNodes() ) {
+    for( var i = 0; i < xml.childNodes.length; i++ ) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if ( typeof(obj[nodeName] ) == "undefined" ) {
+        obj[nodeName] = xmlToJson( item );
+      } else {
+        if ( typeof( obj[nodeName].push ) == "undefined" ) {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push( old );
+        }
+        obj[nodeName].push( xmlToJson( item ) );
+      }
+    }
+  }
+  return obj;
+};
