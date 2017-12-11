5e8c7e1b1e885a5f0e4ff4aa30326ffbda2339f6
marcigarza <marc.igarza@est.fib.upc.edu>
2017-12-11 13:39:32 +0100

    Udates

--- public/scripts/radon_exposure.js
+++ public/scripts/radon_exposure.js
@@ -1,2 +1,18 @@
-console.log(localStorage.getItem("smoke_status"));
+console.log(localStorage.getItem("smoker_status"));
 console.log(localStorage.getItem("age_group"));
+console.log(localStorage.getItem("gender"));
+/*
+smoker_status: 
+				1. current smoker
+				2. ex smoker
+				3. non-smoker
+
+
+age_group:      1. 0  - 50
+ 				2. 50 - 65 
+ 				3. 65+ 
+
+
+gender:         1. male
+				2. female
+*/
--- public/scripts/sensitivity.js
+++ public/scripts/sensitivity.js
@@ -11,9 +11,13 @@ function StoreData() {
 	var smoke_value = e.options[e.selectedIndex].value;
 	e = document.getElementById("age_selection");
 	var age_value = e.options[e.selectedIndex].value;
+	e = document.getElementById("gender_selection");
+	var gender_value = e.options[e.selectedIndex].value;
 	localStorage.setItem("smoker_status", smoke_value);
 	localStorage.setItem("age_group", age_value);
+	localStorage.setItem("gender", gender_value);
 	console.log(age_value);
 	console.log(smoke_value);
+	console.log(gender_value);
 	return returnStatus;
 }
