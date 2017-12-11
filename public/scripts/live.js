4e99395baa5b3f205b89618594fdfcabf34a1437
marcigarza <marc.igarza@est.fib.upc.edu>
2017-12-11 11:29:32 +0100

    Udates

--- public/scripts/live.js
+++ public/scripts/live.js
@@ -26,14 +26,14 @@ socket.on('messages', function(data) {
 function render (data) {  
   var result = data;
   if (JSON.stringify(result.module_1) != undefined) {
-    radon_value_module_1 = JSON.stringify(result.module_1.radon);
-    co2_value_module_1 = JSON.stringify(result.module_1.co2);
-    methane_value_module_1 = JSON.stringify(result.module_1.methane);
+    radon_value_module_1 = clean(JSON.stringify(result.module_1.radon));
+    co2_value_module_1 = clean(JSON.stringify(result.module_1.co2));
+    methane_value_module_1 = clean(JSON.stringify(result.module_1.methane));
   }
   if (JSON.stringify(result.module_2) != undefined) {
-    radon_value_module_2 = JSON.stringify(result.module_2.radon);
-    co2_value_module_2 = JSON.stringify(result.module_2.co2);
-    methane_value_module_2 = JSON.stringify(result.module_2.methane);
+    radon_value_module_2 = clean(JSON.stringify(result.module_2.radon));
+    co2_value_module_2 = clean(JSON.stringify(result.module_2.co2));
+    methane_value_module_2 = clean(JSON.stringify(result.module_2.methane));
   }
 
   document.getElementById(id_radon_value_module_1).innerHTML = radon_value_module_1 + bq;
@@ -42,4 +42,13 @@ function render (data) {
   document.getElementById(id_radon_value_module_2).innerHTML = radon_value_module_2 + bq;
   document.getElementById(id_co2_value_module_2).innerHTML = co2_value_module_2 + ppm;
   document.getElementById(id_methane_value_module_2).innerHTML = methane_value_module_2 + ppm;
+}
+
+
+function clean(data) {
+  var result = data
+  result = result.replace("[", " ");
+  result = result.replace("]", " ");
+  return result;
+
 }
